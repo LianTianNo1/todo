8b977ff0b8cb587abb45ee765132547d41dc1ff9
Lang <1584731441@qq.com>
2024-12-06 16:37:25 +0800

    #fix: 静态导出配置

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     // 这里仅用于开发阶段快速调试
     ignoreBuildErrors: true,
   },
+  output: 'export',  // 添加这行来启用静态导出
+  images: {
+    unoptimized: true, // 静态导出需要这个配置
+  },
 };
 
 export default nextConfig;
